36a08ca854b133bc356a790786bad08a65cb3afd
mosaab-ss <80281522+mosaab-ss@users.noreply.github.com>
2021-03-08 23:25:46 -0700

    Changed GuessRNG containing full game logic

--- src/com/ss/mar/wone/done/GuessRNG.java
+++ src/com/ss/mar/wone/done/GuessRNG.java
@@ -1,5 +1,8 @@
 package com.ss.mar.wone.done;
 
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * A "mini-game" using a RNG with user input guessing the selected number
  * @author Mosaab
@@ -10,6 +13,40 @@ public class GuessRNG {
      * @param args
      */
     public static void main(String[] args) {
-        //TODO: write down the input and other functions
+        /*
+        * We use ThreadLocalRandom because the "older" method of using util.Random can cause contention
+        * Contention can allow seed syncing which in turn can cause the random number to be guessed
+        * This method makes sure that the RNG is used by multiple threads, so seed syncing is difficult/impossible
+        * Source: https://stackoverflow.com/a/363692
+        * */
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, 100+1);
+
+        // Initializing scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Initial prompt
+        System.out.println("I have chosen a number between 1-100, can you guess what it is?");
+        int chances = 5;
+        do {
+            // Wait for user input
+            int guess = scan.nextInt();
+
+            // Input must be within [1-100]
+            if (guess > 100 || guess < 1) {
+                System.out.println("Sorry, you must enter a number in the range of [1-100]. Try again.");
+                continue;
+            }
+
+            // Check if input is within +-10 of the RNG
+            if (guess >= randomNumber -10 && guess <= randomNumber +10) {
+                System.out.println("Nicely done! The number was "+ randomNumber);
+                break;
+            }
+        } while (--chances > 0);
+
+        // Check if the user ran out of chances
+        if (chances == 0) {
+            System.out.println("Sorry, the number was "+ randomNumber + ". Better luck next time!");
+        }
     }
 }
